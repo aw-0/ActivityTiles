3a74ba2883200be6230132c7d94b01fffc076908
andrew <me@andreww.co>
2023-10-14 17:57:58 -0500

    Initial Commit

---
+++ ExerciseWidget/ExerciseWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExerciseWidgetApp.swift
+//  ExerciseWidget
+//
+//  Created by Andrew on 10/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ExerciseWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
